7a0548d96b9df87b2d745845a6103e37b319965c
monitores-de-ingenieria <utesamonitores@outlook.com>
2026-08-12 15:33:25 -0400

    actualizar

--- admin/app.js
+++ admin/app.js
@@ -260,7 +260,7 @@ onAuthStateChanged(auth, async user => {
 });
 
 // ── ROLES Y PERMISOS ──
-const ADMIN_RAIZ = ["utesamonitores@outlook.com"];
+const ADMIN_RAIZ = ["utesamonitores@outlook.com", "motinoresutesa@gmail.com"];
 let rolActual = "administrador";
 let usuarioActualNombre = "Administrador";
 
